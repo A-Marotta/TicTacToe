746468daaf7c09e96a8034b0902ee4cb7eeb0ae2
Adrian <a.marotta97@gmail.com>
2021-05-19 13:33:06 +1000

    init commit and templates finished

---
+++ players.js
@@ -0,0 +1,10 @@
+var player1 = {
+    row1: [null,null,null],
+    row2: [null,null,null],
+    row3: [null,null,null]
+}
+var player2 = {
+    row1: [null,null,null],
+    row2: [null,null,null],
+    row3: [null,null,null]
+}
